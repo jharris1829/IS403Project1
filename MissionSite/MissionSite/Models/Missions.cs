65515ea6bf8a96a7b8ca4f17a3ad228d7dc41922
andersonsmike4 <andersonsmike4@gmail.com>
2017-11-07 19:56:21 -0700

    Model added

---
+++ MissionSite/MissionSite/Models/Missions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MissionSite.Models
+{
+    public class Missions
+    {
+        public string MissionPresident { get; set; }
+        public string MissionAddress { get; set; }
+        public string Language { get; set; }
+        public string Climate { get; set; }
+        public string Religion { get; set; }
+        public string Flag { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string Answer5 { get; set; }
+    }
+}
